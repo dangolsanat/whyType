aff6784150687b1a96faff817fc2dce4659bbb98
dangolsanat <154395036+dangolsanat@users.noreply.github.com>
2024-08-27 23:25:57 -0400

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'secret')  # Replace 'your-static-secret-key' with a fixed key for development
-    SQLALCHEMY_DATABASE_URI = "postgresql:///keygame"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
